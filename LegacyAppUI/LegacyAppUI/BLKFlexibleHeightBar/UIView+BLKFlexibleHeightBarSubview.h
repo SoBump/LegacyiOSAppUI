7f7047424eb158b04837ad5e5d201596889bbbc8
Peyman Khanjan <p.khanjan@gmail.com>
2020-04-04 01:24:36 +0100

    Remove warnings.

--- LegacyAppUI/LegacyAppUI/BLKFlexibleHeightBar/UIView+BLKFlexibleHeightBarSubview.h
+++ LegacyAppUI/LegacyAppUI/BLKFlexibleHeightBar/UIView+BLKFlexibleHeightBarSubview.h
@@ -31,14 +31,11 @@
 
 /**
  Add layout attributes that correspond to a progress value.
- @param The layout attributes that the receiver wants to be applied to itself.
- @param The progress value (between 0.0 and 1.0 inclusive) that the receiver's containing `BLKFlexibleHeightBar` instance will use to decide which layout attributes to apply.
  */
 - (void)addLayoutAttributes:(BLKFlexibleHeightBarSubviewLayoutAttributes *)layoutAttributes forProgress:(CGFloat)progress;
 
 /**
  Remove the layout attributes instance that corresponds to then specified progress value.
- @param The progress value corresponding to the layout attributes that are to be removed.
  */
 - (void)removeLayoutAttributesForProgress:(CGFloat)progress;
 
@@ -50,14 +47,11 @@
 
 /**
  Returns the progress value corresponding to the specified index. This is a helper method for `BLKFlexibleHeightBar.`
- @param The index of the desired progress value.
- @return The progress value corresponding to the specified index.
  */
 - (CGFloat)progressAtIndex:(NSUInteger)index;
 
 /**
  Returns the layout attributes corresponding to the specified index. This is a helper method for `BLKFlexibleHeightBar.`
- @param The index of the desired layout attributes.
  @return The layout attributes corresponding to the specified index.
  */
 - (BLKFlexibleHeightBarSubviewLayoutAttributes *)layoutAttributesAtIndex:(NSUInteger)index;
